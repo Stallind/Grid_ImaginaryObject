4c26bf840d2c5580ebc2e6c8467ec8f23db9ed8c
Stalli <stefan.lindahl87@gmail.com>
2020-04-05 10:58:19 +0200

    Add method to register start object

--- Grid_ImaginaryObject/Program.cs
+++ Grid_ImaginaryObject/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Grid_ImaginaryObject
 {
     class Program
     {
+        public static int ObjectPositionValue { get; set; }
         static void Main(string[] args)
         {
             int[,] grid = GenerateGrid();
@@ -13,6 +15,25 @@ namespace Grid_ImaginaryObject
             {
                 Console.WriteLine(value);
             }
+
+            RegisterInitialObjectPosition(grid);
+        }
+
+        private static void RegisterInitialObjectPosition(int[,] grid)
+        {
+            Console.Write("Object starting position: ");
+            var objectInput = Console.ReadLine();
+
+            //Reading the instructions, I assume you want the input to be typed in this format: x,y
+
+            var splitInput = objectInput.Split(',').Select(int.Parse).ToArray();
+            var objectX = splitInput[0];
+            var objectY = splitInput[1];
+
+            ObjectPositionValue = grid[objectX, objectY];
+
+            var objectStartCords = objectX + objectY.ToString().Insert(0, ",");
+            Console.WriteLine($"Object placed at: {objectStartCords} with the ObjectPositionValue: {ObjectPositionValue}");
         }
 
         private static int[,] GenerateGrid()
